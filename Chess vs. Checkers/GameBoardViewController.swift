b25f621e0aa0b7ad851237605d757ec3724a6dc2
Colin Smith <devcolin@icloud.com>
2019-04-18 11:26:37 -0600

    Initial Commit for Master

---
+++ Chess vs. Checkers/GameBoardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameBoardViewController.swift
+//  Chess vs. Checkers
+//
+//  Created by Colin Smith on 4/17/19.
+//  Copyright © 2019 Colin Smith. All rights reserved.
+//
+
+import UIKit
+
+class GameBoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
